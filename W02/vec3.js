7199c55a465d1cc15a41c25a37b57bcd1b4e00a8
koideharuna <103481276+koideharuna@users.noreply.github.com>
2022-04-13 17:57:05 +0900

    Update vec3.js

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,67 +1,95 @@
-Vec3 = function( x, y, z )
+class Vec3
 {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-}
-
-Vec3 = function( x, y, z )
-{
-    this.x = x;
-    this.y = y;
-    this.z = z;
+    constructor( x, y, z )
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
 
 
-Vec3.prototype.add = function( v )
+class Vec3
 {
-    this.x += v.x;
-    this.y += v.y;
-    this.z += v.z;
-    return this;
+    add( v )
+    {
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
+        return this;
+     }    
 }
 
-Vec3.prototype.sub = function( v )
+
+class Vec3
 {
-    this.x -= v.x;
-    this.y -= v.y;
-    this.z -= v.z;
-    return this;
+    sub( v )
+    {
+        this.x -= v.x;
+        this.y -= v.y;
+        this.z -= v.z;
+        return this;
+     }    
+}
+    
+
+class Vec3
+{
+    sum( )
+    {
+        return this.x + this.y + this.z;    
+     }
+}
+   
+
+class Vec3
+{
+    min( )
+    {
+        //return Math.min( this.x, this.y, this.z );
+        const m =  this.x < this.y ? this.x : this.y;
+        return m < this.z ? m : this.z;
+    }  
 }
 
-Vec3.prototype.sum = function()
+
+class Vec3
 {
-    return this.x + this.y + this.z;
+    max( )
+    {
+        //return Math.max( this.x, this.y, this.z );
+        const m = this.x > this.y ? this.x : this.y;
+        return m > this.z ? this.z : m;
+    }  
 }
 
-Vec3.prototype.min = function()
+
+class Vec3
 {
-    //return Math.min( this.x, this.y, this.z );
-    const m =  this.x < this.y ? this.x : this.y;
-    return m < this.z ? m : this.z;
+    mid( )
+    {
+        return this.sum() - this.min() - this.max();
+    }  
 }
 
-Vec3.prototype.max = function()
-{
-    //return Math.max( this.x, this.y, this.z );
-    const m = this.x > this.y ? this.x : this.y;
-    return m > this.z ? this.z : m;}
 
-Vec3.prototype.mid = function()
+class Vec3
 {
-    return this.sum() - this.min() - this.max();
+    cross( v )
+    {
+        var x = this.x, y = this.y, z = this.z;
+        this.x = y * v.z - z * v.y;
+        this.y = z * v.x - x * v.z;
+        this.z = x * v.y - y * v.x;
+        return this;
+    }  
 }
 
-Vec3.prototype.cross = function( v )
-{
-    var x = this.x, y = this.y, z = this.z;
-    this.x = y * v.z - z * v.y;
-    this.y = z * v.x - x * v.z;
-    this.z = x * v.y - y * v.x;
-    return this;
-}
 
-Vec3.prototype.length = function()
+class Vec3
 {
-    return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
+    length( )
+    {
+        return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
+    }  
 }
