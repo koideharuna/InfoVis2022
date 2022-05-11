3180c889a0c3ae12453b073c2c169c126fcf1161
小出晴菜 <koideharuna@koideharunakenkyuushitsunoMacBook-Pro.local>
2022-05-11 14:25:04 +0900

    Initial commit

--- W08/task1_main.js
+++ W08/task1_main.js
@@ -161,8 +161,8 @@ class ScatterPlot {
             .append("rect")
             .attr("x", 0)
             .attr("y", d => self.yscale(d.label))
-            .attr("width", d => xscale(d.value))
-            .attr("height", yscale.bandwidth());
+            .attr("width", d => self.xscale(d.value))
+            .attr("height", self.yscale.bandwidth());
 
         
         self.xaxis_group
